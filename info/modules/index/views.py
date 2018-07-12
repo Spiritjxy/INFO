666c012b2f50663406bc91a779cb071ec4fdd467
spirit <spiritjxy@gmail.com>
2018-07-12 17:39:48 +0800

    静态文件的引入，返回页面，网页图标的处理

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,6 @@
+from flask import current_app
+from flask import render_template
+
 from . import index_blue
 from info import redis_store
 
@@ -6,4 +9,11 @@ from info import redis_store
 def index():
     redis_store.set('name', 'jxy')
     print(redis_store.get('name'))
-    return 'this is index'
+    return render_template('news/index.html')
+
+
+# 网站logo图片处理,浏览器向每个网站请求数据的时候会默认调用GET 向/favicon.ico接口,获取logo图片
+# current_app.send_static_file('图片'), 会自动去static文件中寻找该图片,返回response对象
+@index_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
