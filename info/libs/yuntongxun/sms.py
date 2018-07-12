7fcc754497ac48cac6edd756cd8f29aad16175d4
spirit <spiritjxy@gmail.com>
2018-07-12 19:10:14 +0800

    发送短信验证码

--- info/libs/yuntongxun/sms.py
+++ info/libs/yuntongxun/sms.py
@@ -3,13 +3,13 @@
 from info.libs.yuntongxun.CCPRestSDK import REST
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
-_accountSid = '8aaf0708568d4143015697b0f4960888'
+_accountSid = '8aaf0708646d63ec01647529e2df0437'
 
 # 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
-_accountToken = '57c6c3ef3cef47e680519a734f6812f8'
+_accountToken = '541975aa2cd04d349d6c3d7a4a20b818'
 
 # 请使用管理控制台首页的APPID或自己创建应用的APPID
-_appId = '8aaf0708568d4143015697b0f56e088f'
+_appId = '8aaf0708646d63ec01647529e338043d'
 
 # 说明：请求地址，生产环境配置成app.cloopen.com
 _serverIP = 'sandboxapp.cloopen.com'
--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,3 +1,6 @@
+import random
+import re
+
 from flask import current_app, jsonify
 from flask import make_response
 from flask import request
@@ -7,6 +10,53 @@ from info import redis_store
 from info.utils.captcha.captcha import captcha
 from info.utils.response_code import RET
 from . import passport_blue
+from info.libs.yuntongxun.sms import CCP
+
+
+# 发送短信验证码
+@passport_blue.route('/sms_code', methods=["POST"])
+def get_sms_code():
+    # 获取参数
+    mobile = request.json.get('mobile')
+    image_code = request.json.get('image_code')
+    image_code_id = request.json.get('image_code_id')
+    # 判断参数是否为空
+    if not all([mobile, image_code, image_code_id]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不完整')
+    # 判断手机号格式
+    if not re.match('1[3456789]\\d{9}', mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg='手机号格式不正确')
+    # 通过验证码id取出验证码
+    try:
+        image = redis_store.get('image_code:%s' % image_code_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='获取图片验证码失败')
+    # 判断验证码是否过期
+    if not image:
+        return jsonify(errno=RET.NODATA, errmsg='图片验证码过期')
+    # 判断输入的验证码是否正确
+    if image_code != image:
+        return jsonify(errno=RET.DATAERR, errmsg='图片验证码不正确')
+    # 生成短信验证码
+    sms_code = '%06d' % random.randint(0, 999999)
+    # 保存验证码在redis
+    try:
+        redis_store.set('sms_code:%s' % mobile, sms_code, constants.SMS_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='保存短信验证码失败')
+    # 调用云通讯发送短信
+    try:
+        ccp = CCP()
+        result = ccp.send_template_sms('18516952650', ['1234', 5], 1)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg='调用云通讯失败')
+    if result != 0:
+        return jsonify(errno=RET.DATAERR, errmsg='发送短信失败')
+
+    return jsonify(errno=RET.OK, errmsg='发送短信成功')
 
 
 # 生成图片验证码
