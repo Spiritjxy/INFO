5b8897d05c98a60726b6bdd4d2f7a8d5c65aa955
spirit <spiritjxy@gmail.com>
2018-07-12 16:05:12 +0800

    编写数据库配置

--- manager.py
+++ manager.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+# 创建配置类
+class Config(object):
+    # 数据库配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/infomation'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+
+# 将配置类信息加载到app
+app.config.from_object(Config)
+# 创建数据库，关联app
+db = SQLAlchemy(app)
 
 
 @app.route('/')
